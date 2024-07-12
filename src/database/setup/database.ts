ec226aa0418b3d1d0bcd2781badd05b9678750ce
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-07-12 14:16:48 +0200

    update

--- src/database/setup/database.ts
+++ src/database/setup/database.ts
@@ -1,15 +1,18 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
+dotenv.config();
+
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env;
 
 // Connect to MySQL using Sequelize
 const todoSequelize = new Sequelize(
   DB_NAME as string,
-  DB_USERNAME as string,
-  DB_PASSWORD,
+  DB_USER as string,
+  DB_PASS,
   {
     host: DB_HOST,
-    dialect: "mysql",
+    dialect: DB_DIALECT as any, // Typzuweisung, um Typfehler zu vermeiden
   }
 );
 
