0a427eea0328702a84e69e43000f42beda08ed84
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-07-12 14:00:45 +0200

    create users.ts & update index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import TodosRouter from "./todos";
+import UsersRouter from "./users";
 
 const AppRouter = Router();
 
 AppRouter.use("/todos", TodosRouter);
+AppRouter.use("/users", UsersRouter);
+
+AppRouter.get("/", (req, res) => {
+  res.status(200).send("Hello, World!");
+});
 
 export default AppRouter;
---
+++ src/routes/users.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import UserModel from "../database/models/UserModel";
+
+const UsersRouter = Router();
+
+// Beispielroute zum Abrufen aller Benutzer
+UsersRouter.get("/", async (req, res) => {
+  try {
+    const users = await UserModel.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export default UsersRouter;
